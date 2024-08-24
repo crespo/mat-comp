b43841d710b5df85704d40aa387c2555703e92b7
raul crespo <raulxcrespo@gmail.com>
2024-08-24 16:57:48 -0300

    temperaturas medias e planilha nova

---
+++ av01/dados_metereologicos.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+arquivo = './dados_inmet.csv'
+planilha = pd.read_csv(arquivo, sep=',')
+
+dados_data = planilha['Data'].tolist()
+dados_precipitacao = planilha['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].tolist()
+dados_temperatura_maxima = planilha['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].tolist()
+dados_temperatura_minima = planilha['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].tolist()
+
+temperaturas_medias = []
+for i in range(len(dados_temperatura_minima)):
+    temperatura_maxima = dados_temperatura_maxima[i]
+    temperatura_minima = dados_temperatura_minima[i]
+    temperatura_media = (temperatura_maxima + temperatura_minima) / 2
+    temperaturas_medias.append(temperatura_media)
+
+
+def diasValidos(lista):
+    lista = [str(i) for i in lista]
+    diasValidos = ['validos']
+    sequencia = 0
+    for item in lista:
+        sequencia += 1
+        if item[0] == 'n':
+            if diasValidos[-1] != 'invalidos':
+                diasValidos.append(sequencia)
+                sequencia = 0
+                diasValidos.append('invalidos')
+        else:
+            if diasValidos[-1] != 'validos':
+                diasValidos.append(sequencia)
+                sequencia = 0
+                diasValidos.append('validos')
+
+    diasValidos.append(sequencia)
+    return diasValidos
+
+
+def verificacoesPorMes(dados_data):
+    linhasPorMeses = []
+    mes_atual = ''
+    linhas = 0
+    for data in dados_data:
+        mes_loop = data[5:7]
+        if mes_atual == '':
+            mes_atual = mes_loop
+            linhas += 1
+        elif mes_atual != mes_loop:
+            linhasPorMeses.append([mes_atual, linhas])
+            mes_atual = mes_loop
+            linhas = 1
+        else:
+            linhas += 1
+
+    linhasPorMeses.append([mes_atual, linhas])
+    return linhasPorMeses
+
+
+diasValidos = diasValidos(temperaturas_medias)
+verificacoesPorMes = verificacoesPorMes(dados_data)
+
+total_mes = 0
+contador = 0
+indice_atual = 0
+temperaturas_por_mes = []
+for temperatura_media in temperaturas_medias:
+    mes_atual = verificacoesPorMes[indice_atual]
+    contador += 1
+    if contador < mes_atual[1]:
+        total_mes += temperatura_media
+    else:
+        temperaturas_por_mes.append([mes_atual[0], round(total_mes / mes_atual[1], 1)])
+        contador = 0
+        indice_atual += 1
+        total_mes = 0
+
+print(temperaturas_por_mes)
+
+# plt.bar(legendas, valores)
+
+
+
+
+
+
+
+
+
+
+
