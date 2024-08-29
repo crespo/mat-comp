b996cc2958c306b9abd607af67890da92d61d977
matheusfillipe-0 <psfilipematheus020@gmail.com>
2024-08-29 15:14:47 -0300

    add mediana, moda e serie historica de precipitacao

--- av01/dados_metereologicos.py
+++ av01/dados_metereologicos.py
@@ -60,22 +60,74 @@ for temperatura_media in temperaturas_medias:
         temperaturas_para_media = []
         contador = 0
         indice_atual += 1
-
-    
+        
+contador = 0
+indice_atual = 0
+precipitacao_por_mes = []
+precipitacao_para_soma = []
+     
+for precipitacao in dados_precipitacao:
+    verificacoes = verificacoesPorMes[indice_atual]
+    contador += 1
+    if contador < verificacoes:
+        if not math.isnan(precipitacao):
+            precipitacao_para_soma.append(precipitacao)
+    else:
+        if precipitacao_para_soma ==[]:
+            precipitacao_por_mes.append(0)
+        else:
+            precipitacao_para_soma = pd.Series(precipitacao_para_soma)
+            precipitacao_por_mes.append(round(precipitacao_para_soma.sum(), 1))
+            
+        precipitacao_para_soma = []
+        contador = 0
+        indice_atual += 1
 
 meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 
 
-plt.bar(meses, temperaturas_por_mes)
+plt.figure(figsize=(14, 6))
+plt.subplot(1, 2, 1)
+plt.bar(meses[:len(temperaturas_por_mes)], temperaturas_por_mes)
+plt.xlabel('Meses')
+plt.ylabel('Temperatura Média (°C)')
+plt.title('Temperatura Média Mensal')
+
+plt.subplot(1, 2, 2)
+plt.bar(meses[:len(precipitacao_por_mes)], precipitacao_por_mes, color='blue')
+plt.xlabel('Meses')
+plt.ylabel('Precipitação Total (mm)')
+plt.title('Precipitação Total Mensal')
+
+plt.tight_layout()
 plt.show()
 
+# média, mediana e moda de temperaturaa.
+temperaturas_series = pd.Series(temperaturas_por_mes)
+media_historica = temperaturas_series.mean()
+mediana_historica = temperaturas_series.median()
+moda_historica = temperaturas_series.mode()
 
 
 
+estatisticas = {
+    'Média': media_historica,
+    'Mediana': mediana_historica,
+    'Moda': moda_historica[0] if not moda_historica.empty else float('nan')  # Usa o primeiro valor da moda se existir
+}
+plt.figure(figsize=(8, 5))
 
 
 
+barras = plt.bar(estatisticas.keys(), estatisticas.values(), color=['orange', 'green', 'blue'])
+plt.xlabel('Estatísticas')
+plt.ylabel('Valor')
+plt.title('Estatísticas Históricas da Temperatura')
 
+for barra in barras:
+    altura = barra.get_height()  
+    plt.text(barra.get_x() + barra.get_width() / 2, altura + 0.5, f'{altura:.2f}', #calcula a posição x que 
+             ha='center', va='bottom')
 
-
-
+plt.tight_layout()
+plt.show()
